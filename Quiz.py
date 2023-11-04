7d845dd07ca1c83eb0d417788da0bbe18ec9628f
Pistacja1212 <zuchowskaola4@gmail.com>
2023-11-04 18:21:52 +0100

    starting quiz program

---
+++ Quiz.py
@@ -0,0 +1,21 @@
+import json;
+import questions.json;
+
+with open('questions.json') as file:
+    data = json.load(file)
+
+for question in questions:
+    print(question)
+    print(answers)
+    x = input('Podaj swoją odpowiedź ')
+    print(x)
+if x == right_answer:
+    print("To prawidłowa odpowiedź")
+else:
+    print("Niestety prawidłowa odpowiedź to ", right_answer)
+
+   
+
+ 
+ 
+   
