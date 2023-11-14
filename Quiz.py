4a487fbdb5e5ad63de871e7a530170c275109482
Pistacja1212 <zuchowskaola4@gmail.com>
2023-11-14 18:19:55 +0100

    My first quiz

--- Quiz.py
+++ Quiz.py
@@ -1,18 +1,40 @@
-import json;
-import questions.json;
+from json import load
+from string import ascii_lowercase
 
-with open('questions.json') as file:
-    data = json.load(file)
+with open('questions.json','r') as file:
+    data = load(file)
+    point = 0
 
-for question in questions:
-    print(question)
-    print(answers)
-    x = input('Podaj swoją odpowiedź ')
-    print(x)
-if x == right_answer:
-    print("To prawidłowa odpowiedź")
-else:
-    print("Niestety prawidłowa odpowiedź to ", right_answer)
+    for question in data:
+        print('pytanie: ', question['question'])
+        print('odpowiedzi:')
+        for letter, answer in zip(ascii_lowercase, question['answers']):
+            print(f'{letter} {answer}')
+
+        print('')       
+        x = input('Podaj swoją odpowiedź ')
+        if x == question['right_answer']:
+            print('')
+            print('Super to poprawna odpowiedz')
+            point += 1
+        else:
+            print('')
+            print('To zla odpowiedz')
+            point -= 1
+    print('') 
+    print(f'Zdobywasz {point} punkty')       
+
+                
+
+
+
+    #     print(answers)
+    #     x = input('Podaj swoją odpowiedź ')
+    #     print(x)
+    # if x == right_answer:
+    #     print("To prawidłowa odpowiedź")
+    # else:
+    #     print("Niestety prawidłowa odpowiedź to ", right_answer)
     
 
    
